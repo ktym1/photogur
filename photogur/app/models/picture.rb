f415157a85dd5fc6c87dae5fc4739c7d5ecaa690
Kerry Mui <kerry.t.mui@gmail.com>
2014-08-18 19:49:35 -0400

    comment model create and working

--- photogur/app/models/picture.rb
+++ photogur/app/models/picture.rb
@@ -1,2 +1,7 @@
 class Picture < ActiveRecord::Base
+	has_many :comments
+	
+	validates :artist, presence: true
+	validates :title, presence: true
+	validates :url, presence: true
 end
--- photogur/db/schema.rb
+++ photogur/db/schema.rb
@@ -11,7 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140818153443) do
+ActiveRecord::Schema.define(version: 20140818232738) do
+
+  create_table "comments", force: true do |t|
+    t.string   "commenter"
+    t.text     "body"
+    t.integer  "picture_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "comments", ["picture_id"], name: "index_comments_on_picture_id"
 
   create_table "pictures", force: true do |t|
     t.string   "artist"
