ed386341054dc60659a5430f6dd579bffcbd7a2c
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-15 19:18:33 -0400

    added initial spec outline tests for picture model

--- spec/models/picture_spec.rb
+++ spec/models/picture_spec.rb
@@ -8,3 +8,13 @@
 #   expect(User.ordered_by_last_name).to eq([chelimsky, lindeman])
 #   end
 # end
+
+require "spec_helper"
+
+describe Picture do
+	it "is valid with a artist, title, and user_id"
+	it "is invalid without an artist"
+	it "is invalid without a title"
+	it "is invalid without a user_id"
+	# it "returns an upvote/downvote? count...?"
+end
