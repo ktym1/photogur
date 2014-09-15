e0a4d486424463ace809d6b13e1964e8e63ad9ee
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-15 18:01:08 -0400

    configured rails-spec test environment

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,18 @@ module Photogur
     # For Foundation 5
     config.assets.precompile += %w( vendor/modernizr )
 
+      config.generators do |g|
+        g.test_framework :rspec,
+            fixtures: true,
+            view_specs: false,
+            helper_specs: false,
+            routing_specs: false,
+            controller_specs: true,
+            request_specs: false
+        g.fixture_replacement :factory_girl, dir: "spec/factories"
+    end
   end
+
+
 end
+
