6f5ef4c4cba8feb90f6f0c0d24249d6c31d43894
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-15 20:21:30 -0400

    refactored pictures controller with before_action method

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,10 @@
 class PicturesController < ApplicationController
+	before_action :get_picture, :only => [:edit, :update, :show, :destroy, :upvote, :downvote]
+
+	def get_picture
+		@picture = Picture.find(params[:id])
+	end
+
 	def index
 		@pictures = Picture.all
 	end
@@ -17,13 +23,7 @@ class PicturesController < ApplicationController
 		end
 	end
 
-	def edit
-		@picture = Picture.find(params[:id])
-	end
-
 	def update
-		@picture = Picture.find(params[:id])
-
 		if @picture.update_attributes(picture_params)
 			redirect_to '/pictures/#{@picture.id}'
 		else
@@ -31,26 +31,25 @@ class PicturesController < ApplicationController
 		end
 	end
 	
-	def show
-		@picture = Picture.find(params[:id])
-	end
-
 	def destroy
-		@picture = Picture.find(params[:id])
 		@picture.destroy
 		redirect_to pictures_url
 	end
 
 	def upvote
-		@picture = Picture.find(params[:id])
 		@picture.liked_by current_user
 		redirect_to @picture
 	end
 
 	def downvote
-		@picture = Picture.find(params[:id])
 		@picture.downvote_from current_user
 		redirect_to @picture
+	end
+
+	def edit
+	end
+
+	def show
 	end
 
 	private
