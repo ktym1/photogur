98cdd58a4eea8fc0aa78e2ee320ef71919aa126f
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-20 15:55:53 -0400

    added passing index pictures controller test

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -3,11 +3,13 @@ require "spec_helper"
 describe PicturesController do
 	
 	describe "GET #index" do
-		# context "with params[:artist]" do
-		# 	it "populates an array of pictures"
-		# 	get :index, artist: "john"
-		# 	expect(assigns(:pictures)).to
-			
+
+		it "assigns @pictures" do
+			picture = create(:picture)
+			get :index
+			expect(assigns(:pictures)).to eq([picture])
+		end
+		
 		it "renders the :index template" do
 			get :index
 			expect(response).to render_template :index
@@ -15,35 +17,35 @@ describe PicturesController do
 	end
 
 
-describe 'GET #show' do
-	it "assigns the requested picture to @picture" do
+	describe 'GET #show' do
+		it "assigns the requested picture to @picture" do
+				picture = create(:picture)
+				get :show, id: picture
+				expect(assigns(:picture)).to eq picture
+			end
+
+		it "renders the :show template" do
 			picture = create(:picture)
 			get :show, id: picture
-			expect(assigns(:picture)).to eq picture
-		end
-
-	it "renders the :show template" do
-		picture = create(:picture)
-		get :show, id: picture
-		expect(response).to render_template :show
-	end
-end
-
-describe "GET #new" do
-	it "assigns a new Picture to @picture"
-	it "renders the :new template"
-	end
-
-describe "POST #create" do
-	context "with valid attributes"do
-		it "saves the new picture in the database"
-		it "redirects to the home page"
-	end
-
-	context "with invalid attributes" do
-		it "does not save the new picture in the database"
-		it "re-renders the :new template"
+			expect(response).to render_template :show
 		end
 	end
+
+	describe "GET #new" do
+		it "assigns a new Picture to @picture"
+		it "renders the :new template"
+		end
+
+	describe "POST #create" do
+		context "with valid attributes"do
+			it "saves the new picture in the database"
+			it "redirects to the home page"
+		end
+
+		context "with invalid attributes" do
+			it "does not save the new picture in the database"
+			it "re-renders the :new template"
+			end
+		end
 
 end
