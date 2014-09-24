d6fe7dfdcd1b44c9632e2c34206e5d3792c944ac
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-24 16:49:15 -0400

    created user factory, and users_spec for feature testing, uncommented authorization rights in comments controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-	# http_basic_authenticate_with name: "kerry", password: "mui", only: :destroy
+	http_basic_authenticate_with name: "kerry", password: "mui", only: :destroy
 	before_action :get_picture, :only => [:create, :destroy]
 
 	def get_picture
---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+FactoryGirl.define.do
+	factory :user do
+		email { Faker::Internet.email }
+		password 'secret'
+		password_confirmation 'secret'
+	end
+end
