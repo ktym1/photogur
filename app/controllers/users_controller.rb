904ecf91e85fdeda1d0cdb5994834ba71b1ca9a6
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-25 10:09:15 -0400

    fixed bug for showing new user view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,8 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @pictures = @user.pictures.paginate(page: params[:page])
-
+  end
+  
   private 
   def user_params
   	params.require(:user).permit(:email, :password, :password_confirmation)
--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,1 +1,13 @@
-users_spec.rb
+require 'spec_helper'
+
+feature 'User management' do 
+	scenario 'adds a new user' do
+
+	visit root_path
+	click_link 'Log in' 
+	fill_in 'Email', with: :user
+	fill_in 'Password', with: :user
+	click_button 'Log in'
+
+	end
+end
