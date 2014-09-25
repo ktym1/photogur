bb7df433b50e56d9117b51c40a685245317c527e
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-25 12:00:33 -0400

    added passing test for signing new user. refactored new user view with class ids

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
 
 feature 'User management' do 
-	scenario 'adds a new user' do
+	scenario 'Logging in' do
 
 	visit root_path
-	click_link 'Log in' 
-	fill_in 'Email', with: :user
-	fill_in 'Password', with: :user
-	click_button 'Log in'
+		click_link 'Log in' 
+		fill_in 'Email', with: :user
+		fill_in 'Password', with: :user
+		click_button 'Log in'
+	end
 
+	scenario 'Signing up' do
+
+	visit root_path
+			click_link 'Sign up'
+			find('.email').fill_in 'Email', with: 'newuser@example.com'
+			find('.password').fill_in 'Password', with: 'secret123'
+			find('.password_confirmation').fill_in 'Password confirmation', with: 'secret123'
+			click_button 'Create User'
 	end
 end
+
+# Logging out
+
+# Add new picture
+# Edit / Delete picture
+
+# Add new comment
+# Edit / Delete comment
