f147dc9516ec85a8c84e343ba1d82065cee4f1fc
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-25 15:41:07 -0400

    extended the passing test for signing in new user to include page redirection, refactored index page accordingly

--- spec/features/users_spec.rb
+++ spec/features/users_spec.rb
@@ -13,11 +13,18 @@ feature 'User management' do
 	scenario 'Signing up' do
 
 	visit root_path
+		expect{	
 			click_link 'Sign up'
 			find('.email').fill_in 'Email', with: 'newuser@example.com'
 			find('.password').fill_in 'Password', with: 'secret123'
 			find('.password_confirmation').fill_in 'Password confirmation', with: 'secret123'
 			click_button 'Create User'
+		}.to change(User,:count).by(1)
+		expect(current_path).to eq pictures_path
+		expect(page).to have_content 'Signed up!'
+			within 'h4' do
+				expect(page).to have_content 'Pictures'
+			end
 	end
 end
 
