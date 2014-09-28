5364aa8130bad812d55edabce8938e1d8b9d59bd
Kerry Mui <kerry.t.mui@gmail.com>
2014-09-28 11:41:34 -0400

    updated show page with large resized display picture

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,6 +36,9 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fill => [200, 200]
   end
 
+  version :display do
+    process :resize_to_fill => [400, 400]
+  end
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
